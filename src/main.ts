c361e2cc34670022cb82336f02ce0479db41c447
Brian Sunter <public@briansunter.com>
2022-06-14 00:13:49 -0700

    feat: configure newlines in settings

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,14 @@ const settingsSchema: SettingSchemaDesc[] = [
     title: "Youtube Captions Language",
     description:
       "What langauge to get captions in. en, es, fr , de etc. See https://wp-info.org/tools/languagecodes.php",
+  },
+  {
+    key: "youtubeCaptionNewlines",
+    type: "boolean",
+    default: false,
+    title: "Youtube Captions Language",
+    description:
+      "Should it put a newline after each caption",
   }
 ];
 
@@ -22,6 +30,11 @@ logseq.useSettingsSchema(settingsSchema);
 
 async function runSubtitlesCommand(b: IHookEvent, cmd: RunOpenYoutubeCommand) {
     const captionLanguage = logseq.settings!["youtubeCaptionLanguage"];
+    const newlines = logseq.settings!["youtubeCaptionNewlines"];
+    let lineSplit = " ";
+    if (newlines) {
+      lineSplit = "\n";
+    }
 
   if (cmd.type === "getall") {
     try {
@@ -37,7 +50,7 @@ async function runSubtitlesCommand(b: IHookEvent, cmd: RunOpenYoutubeCommand) {
           console.log(`getting subtitles for ${youtubeId}`);
           const subs = await getSubtitles({ videoID: youtubeId, lang: captionLanguage });
           if (subs.length > 0) {
-            const allSubtitles = subs.map((s) => s.text).join("\n");
+            const allSubtitles = subs.map((s) => s.text).join(lineSplit);
             await logseq.Editor.insertBlock(currentBlock.uuid, allSubtitles);
           }
         } else {
