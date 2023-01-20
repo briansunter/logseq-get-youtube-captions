4f2a3f0792cb4ad5e6eff2de0653ca8e23611b1b
Brian Sunter <public@briansunter.com>
2023-01-19 17:47:46 -1000

    refactor

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { IHookEvent, SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin";
 import { getSubtitles } from "youtube-captions-scraper";
 import getVideoId from "get-video-id";
 
-interface RunOpenYoutubeCommand {
-  type: "getall";
-}
 const settingsSchema: SettingSchemaDesc[] = [
   {
     key: "youtubeCaptionLanguage",
@@ -21,60 +18,57 @@ const settingsSchema: SettingSchemaDesc[] = [
     type: "boolean",
     default: false,
     title: "Youtube Captions Language",
-    description:
-      "Should it put a newline after each caption",
-  }
+    description: "Should it put a newline after each caption",
+  },
 ];
 
 logseq.useSettingsSchema(settingsSchema);
 
-async function runSubtitlesCommand(b: IHookEvent, cmd: RunOpenYoutubeCommand) {
-    const captionLanguage = logseq.settings!["youtubeCaptionLanguage"];
-    const newlines = logseq.settings!["youtubeCaptionNewlines"];
-    let lineSplit = " ";
-    if (newlines) {
-      lineSplit = "\n";
-    }
+async function getCaptions(b: IHookEvent) {
+  const captionLanguage = logseq.settings!["youtubeCaptionLanguage"];
+  const newlines = logseq.settings!["youtubeCaptionNewlines"];
+  let lineSplit = " ";
+  if (newlines) {
+    lineSplit = "\n";
+  }
 
-  if (cmd.type === "getall") {
-    try {
-      const currentBlock = await logseq.Editor.getBlock(b.uuid);
-      if (currentBlock) {
-        const { id, service } = getVideoId(currentBlock.content);
-        let youtubeId;
-        if (service === "youtube") {
-          youtubeId = id;
-        }
-
-        if (youtubeId) {
-          console.log(`getting subtitles for ${youtubeId}`);
-          const subs = await getSubtitles({ videoID: youtubeId, lang: captionLanguage });
-          if (subs.length > 0) {
-            const allSubtitles = subs.map((s) => s.text).join(lineSplit);
-            await logseq.Editor.insertBlock(currentBlock.uuid, allSubtitles);
-          }
-        } else {
-          console.warn("no youtube id found in block ${currentBlock.content}");
-          logseq.App.showMsg("warn", "No youtube id found in block");
-        }
+  try {
+    const currentBlock = await logseq.Editor.getBlock(b.uuid);
+    if (currentBlock) {
+      const { id, service } = getVideoId(currentBlock.content);
+      let youtubeId;
+      if (service === "youtube") {
+        youtubeId = id;
       }
-    } catch (e) {
-      console.error(e);
-      logseq.App.showMsg("error", "Error getting subtitles");
+
+      if (youtubeId) {
+        console.log(`getting subtitles for ${youtubeId}`);
+        const subs = await getSubtitles({
+          videoID: youtubeId,
+          lang: captionLanguage,
+        });
+        if (subs.length > 0) {
+          const allSubtitles = subs.map((s) => s.text).join(lineSplit);
+          await logseq.Editor.insertBlock(currentBlock.uuid, allSubtitles);
+        }
+      } else {
+        console.warn("no youtube id found in block ${currentBlock.content}");
+        logseq.App.showMsg("warn", "No youtube id found in block");
+      }
     }
-  } else {
-    console.error("Unknown command type");
+  } catch (e) {
+    console.error(e);
+    logseq.App.showMsg("error", "Error getting subtitles");
   }
 }
 
 async function main() {
-  logseq.Editor.registerBlockContextMenuItem("get-youtube-captions", (e) =>
-    runSubtitlesCommand(e, { type: "getall" })
+  logseq.Editor.registerBlockContextMenuItem(
+    "get-youtube-captions",
+    getCaptions
   );
 
-  logseq.Editor.registerSlashCommand("get-youtube-captions", (e) =>
-    runSubtitlesCommand(e, { type: "getall" })
-  );
+  logseq.Editor.registerSlashCommand("get-youtube-captions", getCaptions);
 }
 
 logseq.ready(main).catch(console.error);
